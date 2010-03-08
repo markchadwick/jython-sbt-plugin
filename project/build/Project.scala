4bb94b06a99ab326f4f03871fa63c6553be86e55
Mark Chadwick <mark.chadwick@gmail.com>
2010-03-08 18:16:10 -0500

    Removing testing plugin, upticking to SBT 0.7.1

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,7 +1,6 @@
 import sbt._
 
-class JythonPluginProject(info: ProjectInfo) extends PluginProject(info) 
-                                             with test.ScalaScripted {
+class JythonPluginProject(info: ProjectInfo) extends PluginProject(info) {
   lazy val ivy = "org.apache.ivy" % "ivy" % "2.0.0"
   lazy val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
 }
--- project/plugins/Plugins.scala
+++
@@ -1,5 +0,0 @@
-import sbt._
-
-class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val sbtTest = "org.scala-tools.sbt" % "test" % "0.5.6"
-}
